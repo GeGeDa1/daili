56fc7fdadd1e8251ca703cb469c8d432d13ec5b3
GeGeDa1 <9188354@gmail.com>
2025-01-03 15:24:15 +0800

    更新 uaa.js

--- uaa.js
+++ uaa.js
@@ -5,8 +5,7 @@ if (url.includes("boce.com")) {
     
     // 在这里你可以对所有路径进行处理
     // 比如替换掉广告内容
-    body = body.replace(/<div class="banner_box">[\s\S]*?<\/div>\s*<!-- banner结束 -->/g, '');
-
+    body = body.replace(/<div\s+class="banner_box"[^>]*>[\s\S]*?<\/div>\s*<!-- banner结束 -->/g, '');
 
     
     
