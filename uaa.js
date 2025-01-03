ea8ba047f8b9c8e7a114774cb8a50cfd2fa84d18
GeGeDa1 <9188354@gmail.com>
2025-01-03 15:38:18 +0800

    更新 uaa.js

--- uaa.js
+++ uaa.js
@@ -6,7 +6,7 @@ if (url.includes("uaa.com")) {
     // 在这里你可以对所有路径进行处理
     // 比如替换掉广告内容
     body = body.replace(/<div\s+class="banner_box"[^>]*>[\s\S]*?<\/div>\s*<!-- banner结束 -->/gi, '');
-
+    body = body.replace(/<div\s+class="shortcut_item"[^>]*>[\s\S]*?<\/div>/gi, '');
     
     
 
